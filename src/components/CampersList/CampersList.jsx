6f8b0bb08647514c484a382a96fc3b826846b40f
oleksandra_stepanova <oleksandra.stepanova@gmail.com>
2024-09-29 23:10:50 +0200

    create pagination

--- src/components/CampersList/CampersList.jsx
+++ src/components/CampersList/CampersList.jsx
@@ -2,16 +2,31 @@ import { List, ListItem } from "@mui/material";
 import { useSelector } from "react-redux";
 import { selectCampersItem } from "../../redux/campers/selector";
 import CampersItem from "../CampersItem/CampersItem";
+import { useState } from "react";
+import css from './CampersList.module.css'
 
 export default function CampersList() {
+
     const campers = useSelector(selectCampersItem);
+
+    const [visibleCards, setVisibleCards] = useState(4);
+    const cardPerPage = 4;
+    const loadMoreCard = () => {
+        setVisibleCards((prevVisibleCards) => prevVisibleCards + cardPerPage);
+    }
+    const totalPage = campers.length;   
+
+
     return (
-        <List>
-            {campers.map((camper) => (
-                <ListItem key={camper.id}>
-                    <CampersItem camper={camper} />
-                </ListItem>
-            ))}
-        </List>
+        <section style={{display:'flex',flexDirection:'column',alignItems:'center'}}>
+            <List>
+                {campers.slice(0,visibleCards).map((camper) => (
+                    <ListItem key={camper.id}>
+                        <CampersItem camper={camper} />
+                    </ListItem>
+                ))}
+            </List>
+            {visibleCards<totalPage&&<button onClick={loadMoreCard} className={css.button}>Load more</button>}
+        </section>
     )
 }
